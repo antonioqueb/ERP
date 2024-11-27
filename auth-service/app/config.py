fa110a257da4ef7a1f5e00dafdb1854f90465b42
Antonio Queb <antonioqueb@alphaqueb.online>
2024-11-27 15:38:05 -0600

    Up

--- auth-service/app/config.py
+++ auth-service/app/config.py
@@ -1,15 +1,21 @@
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
+    # Configuración general
     PROJECT_NAME: str = "Gestpro-ERP Auth Service"
     ENVIRONMENT: str = "development"
     API_VERSION: str = "/api/v1"
+    
+    # Configuración de seguridad
     SECRET_KEY: str = "your-secret-key"
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
-    DATABASE_SERVICE_URL: str = "http://db-service:8001/api/v1"
+
+    # Configuración de la base de datos
+    DATABASE_URL: str  # Declaración de la variable para la URL de la base de datos
 
     class Config:
-        env_file = ".env"
+        env_file = ".env"  # Cargar variables desde el archivo .env
 
+# Instancia global de configuración
 settings = Settings()
