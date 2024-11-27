ae4ff1046306b3f84b4416aaa070cb778df7aa7f
Antonio Queb <antonioqueb@alphaqueb.online>
2024-11-27 12:32:19 -0600

    Up

--- gestpro-erp-core/core/config.py
+++ gestpro-erp-core/core/config.py
@@ -7,11 +7,7 @@ class Settings(BaseSettings):
     AUTH_SERVICE_URL: str = "http://auth-service:8002/api/v1"
     API_VERSION: str = "/api/v1"
 
-    # Configuración de logs
-    LOG_LEVEL: str = Field(default="INFO", env="LOG_LEVEL")
-    LOG_FILE: str = Field(default="gestpro-erp-core.log", env="LOG_FILE")
-    DEBUG: bool = Field(default=True, env="DEBUG")
-    TIMEOUT: int = Field(default=30, env="TIMEOUT")
+
 
     class Config:
         env_file = ".env"
