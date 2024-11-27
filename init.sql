dce6ed4f86ef1bfb6215142a64d825e85d24ca89
Antonio Queb <antonioqueb@alphaqueb.online>
2024-11-27 15:18:00 -0600

    Up

--- init.sql
+++ init.sql
@@ -1,2 +1,17 @@
-CREATE ROLE "user" WITH LOGIN PASSWORD 'password';
-CREATE DATABASE "gestpro_db" OWNER "user";
+DO
+$$
+BEGIN
+   IF NOT EXISTS (SELECT FROM pg_catalog.pg_roles WHERE rolname = 'user') THEN
+      CREATE ROLE "user" WITH LOGIN PASSWORD 'password';
+   END IF;
+END
+$$;
+
+DO
+$$
+BEGIN
+   IF NOT EXISTS (SELECT FROM pg_catalog.pg_database WHERE datname = 'gestpro_db') THEN
+      CREATE DATABASE "gestpro_db" OWNER "user";
+   END IF;
+END
+$$;
