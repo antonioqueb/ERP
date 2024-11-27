061558acf2552a5004cc317fe341328042d34e09
Antonio Queb <antonioqueb@alphaqueb.online>
2024-11-27 16:12:12 -0600

    Up

--- auth-service/app/config.py
+++ auth-service/app/config.py
@@ -4,13 +4,14 @@ class Settings(BaseSettings):
     PROJECT_NAME: str = "Gestpro-ERP Auth Service"
     ENVIRONMENT: str = "development"
     API_VERSION: str = "/api/v1"
-    SECRET_KEY: str = "your-secret-key"
+    SECRET_KEY: str  # Será obligatorio configurarlo en `.env`
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
-    DATABASE_URL: str = "postgresql://user:password@db-service:5432/gestpro_db"
-    AUTH_SERVICE_URL: str = "http://auth-service:8002/api/v1"  # Añadir esta línea
+    DATABASE_URL: str  # Será obligatorio configurarlo en `.env`
+    AUTH_SERVICE_URL: str = "http://auth-service:8002/api/v1"
 
     class Config:
-        env_file = ".env"
+        env_file = ".env"  # Carga valores desde un archivo .env
 
+# Instanciar la configuración
 settings = Settings()
