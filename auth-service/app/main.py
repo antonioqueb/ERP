42b73d78cc5f0f9e68275a6fe0a24bfb0e05b0bc
Antonio Queb <antonioqueb@alphaqueb.online>
2024-11-27 12:40:29 -0600

    Up

--- auth-service/app/main.py
+++ auth-service/app/main.py
@@ -2,12 +2,16 @@ from fastapi import FastAPI
 from app.config import settings
 from app.events import on_startup, on_shutdown
 from app.logging_config import setup_logging
-from app.routers import auth
+from app.routers import auth  # Importa el router de autenticación
 
 # Configurar logging
 setup_logging()
 
 app = FastAPI(title=settings.PROJECT_NAME)
+
+@app.get("/health")
+async def health_check():
+    return {"status": "auth-service is healthy"}
 
 @app.on_event("startup")
 async def startup_event():
