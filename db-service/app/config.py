2b76d83386cc7a1a7c4e6be56ab93e7aa3f8b68f
Antonio Queb <antonioqueb@alphaqueb.online>
2024-11-27 18:25:04 -0600

    Up

--- db-service/app/config.py
+++ db-service/app/config.py
@@ -1,12 +1,14 @@
 # db-service/app/config.py
 
 from pydantic_settings import BaseSettings
+from pydantic import Field
 
 
 class Settings(BaseSettings):
     PROJECT_NAME: str = "Gestpro-ERP Database Service"
     ENVIRONMENT: str = "development"
     API_VERSION: str = "/api/v1"
+    database_url: str = Field(..., env='DATABASE_URL')
 
     class Config:
         env_file = ".env"
