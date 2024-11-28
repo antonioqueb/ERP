d9b31e33ff14c9e4e62a467b86a3eef131567761
Antonio Queb <antonioqueb@alphaqueb.online>
2024-11-27 19:06:47 -0600

    Up

--- db-service/app/main.py
+++ db-service/app/main.py
@@ -9,15 +9,18 @@ from app.logging_config import setup_logging
 # Configurar logging
 setup_logging()
 
+# Crear la instancia de la aplicación FastAPI
 app = FastAPI(title=settings.project_name)
-
 
 @app.on_event("startup")
 async def startup_event():
+    """Evento de inicio de la aplicación."""
     await on_startup(app)
 
 @app.on_event("shutdown")
 async def shutdown_event():
+    """Evento de cierre de la aplicación."""
     await on_shutdown(app)
 
-app.include_router(health.router, prefix=settings.API_VERSION)
+# Registrar el router de salud
+app.include_router(health.router, prefix=settings.api_version)
